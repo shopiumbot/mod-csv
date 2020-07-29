07e9bea893653eb93765b5dca172bde9f87530b1
Andrew <andrew.panix@gmail.com>
2020-07-29 20:43:25 +0300

    init

--- migrations/m170908_104527_csv.php
+++ migrations/m170908_104527_csv.php
@@ -11,7 +11,7 @@ namespace shopium\mod\csv\migrations;
  * Class m170908_104527_csv
  */
 
-use panix\engine\db\Migration;
+use yii\db\Migration;
 
 class m170908_104527_csv extends Migration
 {
@@ -19,13 +19,13 @@ class m170908_104527_csv extends Migration
 
     public function up()
     {
-
+        $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci ENGINE=InnoDB';
         $this->createTable('{{%csv}}', [
             'id' => $this->primaryKey()->unsigned(),
             'object_id' => $this->integer()->null(),
             'object_type' => $this->tinyInteger()->null(),
             'external_id' => $this->string(255)->null(),
-        ], $this->tableOptions);
+        ], $tableOptions);
 
         $this->createIndex('object_id', '{{%csv}}', 'object_id');
         $this->createIndex('object_type', '{{%csv}}', 'object_type');
--- views/settings/index.php
+++ views/settings/index.php
@@ -5,9 +5,37 @@ use panix\engine\bootstrap\ActiveForm;
 use shopium\mod\shop\models\ProductType;
 use yii\helpers\ArrayHelper;
 
-/**
- *
- */
+$str = '=IMAGE("https://sun9-22.userapi.com/c855128/v855128088/114004/x7FdunGhaWc.jpg",2)';
+
+//$preg = preg_match('/=(image|IMAGE)(.*)/',$str,$match);
+$preg = preg_match('/=(image|IMAGE)(.*)/',$str,$match);
+
+\panix\engine\CMS::dump($preg);
+\panix\engine\CMS::dump($match);
+/*
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load(Yii::getAlias('@runtime').DIRECTORY_SEPARATOR.'google.xlsx');
+$worksheet = $spreadsheet->getSheet(1);
+//$props = $spreadsheet->getProperties();
+$rows = [];
+
+foreach ($worksheet->getRowIterator() as $row) {
+    $cellIterator = $row->getCellIterator();
+    $cellIterator->setIterateOnlyExistingCells(false); // This loops through all cells,
+    $cells = [];
+    foreach ($cellIterator as $k => $cell) {
+        if($cell->getDataType() == 'f'){
+
+            $cells[] = 'F='.$cell->getValue();
+        }else{
+            $cells[] = $cell->getValue();
+        }
+
+    }
+    $rows[] = $cells;
+}
+
+
+\panix\engine\CMS::dump($rows);*/
 ?>
 
 <div class="card">
