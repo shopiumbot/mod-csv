30b086596d5a2ee1177ddb644eba7eddc6c4b859
Andrew <andrew.panix@gmail.com>
2020-11-14 12:44:01 +0200

    ``

---
+++ messages/en/SettingsForm.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'PAGENUM' => 'The number of items in the export file',
+    'INDENT_ROW' => 'Starting line',
+    'INDENT_COLUMN' => 'Starting column',
+    'IGNORE_COLUMNS' => 'Ignore columns',
+    'GOOGLE_SHEET_ID' => 'Table ID',
+    'GOOGLE_SHEET_LIST' => 'Table List',
+];
---
+++ messages/uk/SettingsForm.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'PAGENUM' => 'Кількість товарів у файлі експорту',
+    'INDENT_ROW' => 'Початкова рядок',
+    'INDENT_COLUMN' => 'Початковий стовпець',
+    'IGNORE_COLUMNS' => 'Ігнорувати стовпці',
+    'GOOGLE_SHEET_ID' => 'ID таблиці',
+    'GOOGLE_SHEET_LIST' => 'Список таблиці',
+];
